3484c00e2cf484b9415396b3d8d022d689824a37
nonya <nonya@hostname>
2025-12-15 11:48:41 +0000

    The anigen files from school project

---
+++ anigen/blog.py
@@ -0,0 +1,134 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for, current_app
+)
+from werkzeug.exceptions import abort
+from anigen.auth import login_required
+from anigen.db import get_db
+from PIL import Image
+
+import json
+import requests
+import io
+
+API_URL = "" # Insert desired model from HuggingFace here
+API_TOKEN = "" # Insert your own token here!
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+from uuid import uuid4
+import os
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, seed, created, author_id, username, filepath'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    print(posts)
+    return render_template('blog/index.html', posts=posts)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        seed = request.form['seed']
+        prompt = request.form['prompt']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        image_bytes = query({
+	        "inputs": prompt,
+        })
+
+        image = Image.open(io.BytesIO(image_bytes))
+
+        path = os.path.join(current_app.config['STATIC_FOLDER']+"images/", str(g.user['id']))
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        ident = uuid4().__str__()
+        filename = f"ani_{ident}.png"
+
+        filepath = str(g.user['id']) + "/" + filename
+        image.save(current_app.config['STATIC_FOLDER']+"images/"+str(g.user['id'])+"/"+filename)
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, seed, prompt, filepath, author_id)'
+                ' VALUES (?, ?, ?, ?, ?)',
+                (title, seed, prompt, filepath, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, seed, created, author_id, username, filepath'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    # SELECT p.id, title, seed, created, author_id, username, filepath
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        seed = request.form['seed']
+        prompt = request.form['prompt']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, seed = ?, prompt = ?'
+                ' WHERE id = ?',
+                (title, seed, prompt, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    # post = get_post(id)
+    get_post(id)
+    # os.remove(post[-1]) -> Removes the image from directory, before we delete the post!
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
